2c93fe661c7e82f869f7f263618b94ee565590e3
manav-chotai <manav.chotai128386@marwadiuniversity.ac.in>
2025-08-09 16:30:41 +0530

    Add files via upload

---
+++ postlab1.py
@@ -0,0 +1,6 @@
+length = float(input("Enter length of rectangle: "))
+width = float(input("Enter width of rectangle: "))
+area = length * width
+perimeter = 2 * (length + width)
+print("Area of Rectangle:", area)
+print("Perimeter of Rectangle:", perimeter)
