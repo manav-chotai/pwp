462992c0d69324944825aff6f3451efbd8010e44
manav-chotai <manav.chotai128386@marwadiuniversity.ac.in>
2025-11-19 15:06:49 +0530

    Refactor QR code scanner for improved readability

--- python_project/project3.py
+++ python_project/project3.py
@@ -1,39 +1,35 @@
 import cv2
-import numpy as np
 from pyzbar.pyzbar import decode
 
-cap = cv2.VideoCapture(0)
-last_data = None
-
-print("Scanning for QR Code. Press 'ESC' to quit.")
+camera = cv2.VideoCapture(0)
+print("QR Scanner running... press ESC to quit")
+previous_text = None
 
 while True:
-    success, frame = cap.read()
-    if not success:
-        break
 
-    for barcode in decode(frame):
-        data = barcode.data.decode('utf-8')
+  ret, frame = camera.read()
+  if not ret:
+    print("Unable to access camera")
+    break
 
-        if data != last_data:
-            print("QR Code data:", data)
-            last_data = data
+  results = decode(frame)
 
-        points = barcode.polygon
-        if len(points) > 4:
-            hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
-            hull = list(map(tuple, np.squeeze(hull)))
-        else:
-            hull = points
-        for i in range(len(hull)):
-            cv2.line(frame, hull[i], hull[(i+1) % len(hull)], (255, 0, 0), 3)
+  for r in results:
+    text = r.data.decode("utf-8")
+    if text != previous_text:
+      print("Scanned:", text)
+      previous_text = text
 
-    cv2.imshow("QR Code Scanner", frame)
+    pts = r.polygon
 
-    key = cv2.waitKey(10)
-    if key == 27 or key == ord('q'):
-        break
+    for idx in range(len(pts)):
+      p1 = pts[idx]
+      p2 = pts[(idx + 1) % len(pts)]
+      cv2.line(frame, p1, p2, (0, 0, 255), 2)
 
+  cv2.imshow("QR Scanner", frame)
+  if cv2.waitKey(1) == 27:
+    break
 
-cap.release()
+camera.release()
 cv2.destroyAllWindows()
